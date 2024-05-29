fab91c6cffc4211b9c8ac03b2e5f0751e9acffbf
sadiakanwal10 <sadiakanwal715@gmail.com>
2024-05-28 23:57:58 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// ADDITION //
+let x = 10;
+let y = 2;
+let sum = x + y;
+console.log("addition:", sum);
+// SUBTRACTION //
+let subtract = x - y;
+console.log("subtraction:", subtract);
+// MULTIPLICATION //
+let multiply = x * y;
+console.log("multiplication:", multiply);
+// DIVISION //
+let divide = x / y;
+console.log("division:", divide);
+// modulus //
+let percentage = x % y;
+console.log("modulus:", percentage);
+export {};
---
+++ main.ts
@@ -0,0 +1,17 @@
+// ADDITION //
+let x:number = 10;
+let y:number = 2;
+let sum:number = x+y;
+console.log("addition:", sum);
+// SUBTRACTION //
+let subtract:number = x-y;
+console.log("subtraction:", subtract);
+// MULTIPLICATION //
+let multiply:number = x*y;
+console.log("multiplication:", multiply);
+// DIVISION //
+let divide:number = x/y;
+console.log("division:", divide);
+// modulus //
+let percentage:number = x%y;
+console.log("modulus:", percentage);
